ca41b963133e90b3f78fdb4cd63bd52dac52e0f6
Noah Levin <50681582+nllevin@users.noreply.github.com>
2019-05-21 15:25:58 -0400

    Add keypress.rb code

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,6 +1,7 @@
 require_relative "board"
 require_relative "tile"
 require "yaml"
+requrire "io/console"
 
 class MinesweeperGame
     def initialize(height, width, mines)
@@ -118,6 +119,22 @@ class MinesweeperGame
         rescue
             return nil
         end
+    end
+
+    def read_char
+        STDIN.echo = false
+        STDIN.raw!
+
+        input = STDIN.getc.chr
+        if input == "\e" then
+            input << STDIN.read_nonblock(3) rescue nil
+            input << STDIN.read_nonblock(2) rescue nil
+        end
+        ensure
+        STDIN.echo = true
+        STDIN.cooked!
+
+        return input
     end
 end
 
